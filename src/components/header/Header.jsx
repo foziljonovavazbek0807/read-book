a2d47e61a8754d33a8d860eba9c895409e704a23
Avazbek <foziljonovavazbek0807@gmail.com>
2022-03-08 22:15:38 +0500

    start header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import "./Header.css";
+import {FaOpencart} from "react-icons/fa"
 
 function Header() {
   return (
     <div className="Header">
-        
+      <div className="header_side">
+        <b>Welcome to reading books</b>
+        <h2>
+          Buy almost all types of books here. You can also sell us any book you
+          have. Delivery is free!!!
+        </h2>
+        <div className="header_btn_block">
+          <a href="#!">
+            Buy  
+            <FaOpencart className="cart_icon"/>
+          </a>
+          <a href="#!">
+            Sell
+            <FaOpencart className="cart_icon"/>
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Navbar.css";
 import { FaSignInAlt } from 'react-icons/fa';
+import { IoIosSearch } from 'react-icons/io';
 
 function Navbar() {
   return (
@@ -10,12 +11,10 @@ function Navbar() {
             <img src="../../images/web-logo.png" alt="logo"/>
             <b>BOOK</b>
         </a>
-        <ul className="Menu">
-            <li><a href="#!">Home</a></li>
-            <li><a href="#!">Home</a></li>
-            <li><a href="#!">Home</a></li>
-            <li><a href="#!">Home</a></li>
-        </ul>
+        <form action="#!" className="search_input">
+          <input type="text" placeholder="Search" />
+          <IoIosSearch className="search_icon"/>
+        </form>
         <button className="BtnInter">
           <FaSignInAlt className="InterIcon"/>
           <p>Register</p>
@@ -24,4 +23,4 @@ function Navbar() {
   )
 }
 
-export default Navbar
+export default Navbar;
